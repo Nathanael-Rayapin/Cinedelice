1fc0937fd7426a5bdb5b52693a396057e20edf45
arfaouimehdi <mehdi.arfaoui@oclock.school>
2025-11-03 17:48:37 +0100

    feat:ajout du status 200

--- backend/src/controllers/recipes.controller.ts
+++ backend/src/controllers/recipes.controller.ts
@@ -1,7 +1,9 @@
+// On importe notre client Prisma déjà configuré (connexion à la base de données)
 import { prisma } from "../models/index.ts";
+// On importe les types Request et Response pour mieux typer Express
 import type { Request,Response } from "express";
 
 export async function getAllRecipes(req:Request,res:Response){
   const recipes = await prisma.recipe.findMany();
-  res.json(recipes);
+  res.status(200).json(recipes);
 }
