d56a7cb7478fdc65e9d8cf97818af07b09769e8a
toscanluc <luc.toscan@gmail.com>
2025-11-05 09:27:23 +0100

    feat: /admin/categories > route et controller

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { router as recipesRouter} from "../routers/recipes.router.ts";
 import { router as authRouter} from "../routers/auth.router.ts";
 import { router as usersRouter} from "../routers/users.router.ts";
+import { router as categoriesRouter } from "../routers/categories.router.ts";
 
 export const router=Router();
 
 router.use(recipesRouter);
 router.use(usersRouter);
 router.use(authRouter);
-//router.use(authRouterRouter);
+router.use(categoriesRouter);
