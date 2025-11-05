e0fb39ac3ef533e2bdb35ef13eb2ad1466eca2f4
toscanluc <luc.toscan@gmail.com>
2025-11-05 10:36:33 +0100

    feat: /categories/:id > route et controller

--- backend/src/controllers/categories.controller.ts
+++ backend/src/controllers/categories.controller.ts
@@ -1,7 +1,20 @@
+// import * as z from "zod";
 import type { Request, Response } from "express";
 import { prisma } from "../models/index.ts";
+import { NotFoundError } from "../lib/errors.ts";
 
 export async function getAllCategories(req: Request, res: Response) {
   const categories = await prisma.category.findMany();
   res.json(categories);
+}
+
+export async function getOneCategory(req: Request, res: Response) {
+  // on récupère l'ID du categorie qui nous intéresse en BDD
+  const categoryId = parseInt(req.params.id, 10);
+  
+  // On récupère la catégorie en BDD, si elle n'existe pas => 404
+  const category = await prisma.category.findUnique({ where: {id: categoryId }});
+  if (!category) { throw new NotFoundError("Category not found"); }
+
+  res.json(category);
 }
--- backend/src/routers/categories.router.ts
+++ backend/src/routers/categories.router.ts
@@ -3,4 +3,5 @@ import * as categoriesController from "../controllers/categories.controller.ts";
 
 export const router = Router();
 
-router.get("/admin/categories", categoriesController.getAllCategories);
+router.get("/categories", categoriesController.getAllCategories);
+router.get("/categories/:id", categoriesController.getOneCategory);
