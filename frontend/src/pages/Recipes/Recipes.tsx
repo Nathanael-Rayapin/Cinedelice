0214657597f8f49f02357b2e35cfdc07fdd43195
ChristopheCoustillas <christophe.coustillas@oclock.school>
2025-11-06 14:45:50 +0100

    feat: raccordement page recettes à la bdd en cours

--- frontend/src/pages/Recipes/Recipes.tsx
+++ frontend/src/pages/Recipes/Recipes.tsx
@@ -3,14 +3,13 @@ import { useEffect, useState } from 'react'; //Hooks essentiels pour gérer l'é
 import { getRecipes } from '../../services/recipes'; //Fonction asynchrone pour récupérer les recettes depuis un service
 import { type IRecipe } from '../../interfaces/recipe'; //Interface TypeScript pour typer les données des recettes
 import PacmanLoader from 'react-spinners/PacmanLoader'; //Composant de chargement visuel
-import FeaturedCard from '../../components/Featured-Card/Featured-Card'; //Composant personnalisé pour afficher chaque recette        
+import FeaturedCard from '../../components/Featured-Card/Featured-Card'; //Composant personnalisé pour afficher chaque recette    
+ 
 
 const Recipes = () => {
     const [recipes, setRecipes] = useState<IRecipe[]>([]); //Tableau de recettes (initialisé comme tableau vide)
     const [errorMsg, setErrorMsg] = useState<string | null>(null); //Message d'erreur (initialisé à null)
     const [loading, setLoading] = useState(true); //Booléen pour indiquer si les données sont en cours de chargement
-    const [currentPage, setCurrentPage] = useState(1); //Page actuelle pour la pagination
-    const recipesPerPage = 6; //Nombre de recettes à afficher par page
 
     //Le useEffect est utilisé pour charger les recettes au montage du composant (tableau de dépendances vide [])
     useEffect(() => {
@@ -30,27 +29,6 @@ const Recipes = () => {
         fetchRecipes();
     }, []);
 
-    // Logique de pagination
-    const indexOfLastRecipe = currentPage * recipesPerPage; //Calcule l'index (position) de la dernière recette à afficher sur la page actuelle
-    const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage; //Calcule l'index (position) de la première recette à afficher sur la page actuelle
-    const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe); //Extrait un sous-tableau de recettes à afficher sur la page actuelle, en utilisant les index calculés précédemment
-    const totalPages = Math.ceil(recipes.length / recipesPerPage); //Calcule le nombre total de pages nécessaires pour afficher toutes les recettes
-
-    const paginate = (pageNumber: number) => setCurrentPage(pageNumber); // Fonction qui met à jour l'état currentPage avec le numéro de page passé en argument
-
-    //Passe à la page précédente si elle existe
-    const goToPreviousPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
-    };
-
-    //Passe à la page suivante si elle existe
-    const goToNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
-    };
 
     //Si loading est true, un composant PacmanLoader est affiché avec une couleur orange (#fB8b24)
     if (loading) {
@@ -74,49 +52,10 @@ const Recipes = () => {
     //Si tout va bien, les recettes sont affichées sous forme de cartes (FeaturedCard) en utilisant recipes.map
     return (
         <div className="recipes">
-            {currentRecipes.map((recipe) => (
+            {recipes.map((recipe) => (
                 <FeaturedCard key={recipe.id} recipe={recipe} />
             ))}
 
-             {/* Boutons de pagination */}
-            <div className="pagination">
-                <button
-                    onClick={goToPreviousPage}
-                    disabled={currentPage === 1}
-                    className="pagination-button"
-                >
-                    Précédent
-                </button>
-
-                {/* Génère dynamiquement un bouton pour chaque numéro de page (de 1 à totalPages) */}
-                {/* Crée un tableau de nombres de 1 à totalPages (ex: [1, 2, 3, ..., totalPages]) */}
-                {/* Parcourt ce tableau et génère un bouton pour chaque numéro de page */}
-                {/* key={number} : Clé unique pour chaque bouton (obligatoire dans une liste React).
-                onClick={() => paginate(number)} :
-                Appelle la fonction paginate avec le numéro de page (number) pour mettre à jour currentPage.
-                className={currentPage === number ? 'active' : ''} :
-                Applique la classe active au bouton si currentPage correspond au numéro de page (number).
-                Cela permet de mettre en évidence la page actuelle (ex: couleur de fond différente). */}
-                {Array.from({ length: totalPages }, (_, i) => i + 1).map((number) => (
-                    <button
-                        key={number}
-                        onClick={() => paginate(number)}
-                        className={currentPage === number ? 'active' : ''} 
-                    >
-                        {number}
-                    </button>
-                ))}
-
-
-                {/* Permet à l'utilisateur d'aller à la page suivante */}
-                <button
-                    onClick={goToNextPage}
-                    disabled={currentPage === totalPages}
-                    className="pagination-button"
-                >
-                    Suivant
-                </button>
-            </div>
         </div>
     );
 };
