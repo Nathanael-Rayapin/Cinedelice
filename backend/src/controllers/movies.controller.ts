5176a092ea340b106cb8239eb3317098cc8ff0e5
toscanluc <luc.toscan@gmail.com>
2025-11-06 17:16:23 +0100

    feat: api/movies (preparation du terrain)

---
+++ backend/src/controllers/movies.controller.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from "express";
+// import { prisma } from "../models/index.ts";
+// import { BadRequestError, ConflictError, NotFoundError } from "../lib/errors.ts";
+
+export async function getAllMovies(req: Request, res: Response) {
+}
+
+export async function getOneMovie(req: Request, res: Response) {
+}
+
+export async function deleteMovie(req: Request, res: Response) {
+}
---
+++ backend/src/routers/movies.router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as moviesController from "../controllers/movies.controller.ts";
+import { checkRoles } from "../middlewares/access-control-middleware.ts";
+
+export const router = Router();
+
+router.get("/movies", checkRoles(["user", "admin"]), moviesController.getAllMovies);
+router.get("/movies/:id", checkRoles(["user", "admin"]), moviesController.getOneMovie);
+router.delete("/movies/:id", checkRoles(["user", "admin"]), moviesController.deleteMovie);
