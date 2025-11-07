96584175d76f7c44a9dd9012b31e1d7add42545e
toscanluc <luc.toscan@gmail.com>
2025-11-07 13:57:48 +0100

    fix: Suppression du middleware checkRoles pour la récupération des categories

--- backend/src/routers/categories.router.ts
+++ backend/src/routers/categories.router.ts
@@ -4,8 +4,8 @@ import { checkRoles } from "../middlewares/access-control-middleware.ts";
 
 export const router = Router();
 
-router.get("/categories", checkRoles(["user", "admin"]), categoriesController.getAllCategories);
-router.get("/categories/:id", checkRoles(["user", "admin"]), categoriesController.getOneCategory);
+router.get("/categories", categoriesController.getAllCategories);
+router.get("/categories/:id", categoriesController.getOneCategory);
 router.post("/categories", checkRoles(["admin"]), categoriesController.createCategory);
 router.patch("/categories/:id", checkRoles(["admin"]), categoriesController.updateCategory);
 router.delete("/categories/:id", checkRoles(["admin"]), categoriesController.deleteCategory);
