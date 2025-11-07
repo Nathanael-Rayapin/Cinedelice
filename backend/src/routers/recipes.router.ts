2ddaa40e814bb3f10567681ec69bfd2894807fca
Nathanaël-Rayapin <nathanael.rayapin@gmail.com>
2025-11-07 13:21:34 +0100

    fix: Suppression du middleware checkRoles pour la récupération de recettes

--- backend/src/routers/recipes.router.ts
+++ backend/src/routers/recipes.router.ts
@@ -4,8 +4,8 @@ import { checkRoles } from "../middlewares/access-control-middleware.ts";
 
 export const router = Router();
 
-router.get("/recipes", checkRoles(["user", "admin"]), recipesController.getAllRecipes);
-router.get("/recipes/:id", checkRoles(["user", "admin"]), recipesController.getOneRecipe);
+router.get("/recipes", recipesController.getAllRecipes);
+router.get("/recipes/:id", recipesController.getOneRecipe);
 router.post("/recipes", checkRoles(["user", "admin"]), recipesController.createRecipe);
 router.patch("/recipes/:id", checkRoles(["user", "admin"]), recipesController.updateRecipe);
 router.delete("/recipes/:id", checkRoles(["user", "admin"]), recipesController.deleteRecipe);
