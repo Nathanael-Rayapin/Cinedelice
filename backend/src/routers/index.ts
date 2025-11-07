a833e38b8937440bc0cf92df3db74829b66d2812
Nathanaël-Rayapin <nathanael.rayapin@gmail.com>
2025-11-07 13:18:01 +0100

    fix: Décommenter le router pour les recettes

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { router as authRouter} from "../routers/auth.router.ts";
 import { router as categoriesRouter } from "../routers/categories.router.ts";
 import { router as usersRouter} from "./users.router.ts";
+import { router as recipesRouter} from "./recipes.router.ts";
 
 export const router=Router();
 
-//router.use(recipesRouter);
+router.use(recipesRouter);
 router.use(usersRouter);
 router.use(authRouter);
 router.use(categoriesRouter);
