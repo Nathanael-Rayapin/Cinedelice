2ef484b3a2719270847852e3445c2dbc9f5c8a35
toscanluc <luc.toscan@gmail.com>
2025-11-10 11:17:28 +0100

    feat: DELETE /api/recipes/me/:id ajout route pour supprimer mes recettes

--- backend/src/routers/recipes.router.ts
+++ backend/src/routers/recipes.router.ts
@@ -6,11 +6,12 @@ export const router = Router();
 
 router.get("/recipes/me",checkRoles(["user", "admin"]), recipesController.getAllMyRecipes);
 router.get("/recipes/me/:id",checkRoles(["user", "admin"]), recipesController.getMyRecipe);
+router.patch("/recipes/me/:id", checkRoles(["user", "admin"]), recipesController.updateMyRecipe);
+router.delete("/recipes/me/:id", checkRoles(["user", "admin"]), recipesController.deleteMyRecipe);
 
 router.get("/recipes", recipesController.getAllRecipes);
 router.get("/recipes/:id", recipesController.getOneRecipe);
 
-router.patch("/recipes/me/:id", checkRoles(["user", "admin"]), recipesController.updateMyRecipe);
 router.delete("/recipes/:id", checkRoles(["admin"]), recipesController.deleteAnyRecipe);
 
 router.post("/recipes", checkRoles(["user", "admin"]), recipesController.createRecipe);
