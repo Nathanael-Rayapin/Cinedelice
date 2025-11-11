b0bf305dc6dec73122eedc8d2e296171e221c0a2
toscanluc <luc.toscan@gmail.com>
2025-11-11 18:26:13 +0100

    feat: feat: GET /api/recipes (validation query params: ajout movieId)

--- backend/src/validations/recipes.validation.ts
+++ backend/src/validations/recipes.validation.ts
@@ -6,6 +6,7 @@ export async function validateGetAllRecipesQueryParams(query: unknown) {
     return await z.object({ 
       userId: z.coerce.number().int().positive().optional(),
       categoryId: z.coerce.number().int().positive().optional(),
+      movieId: z.coerce.number().int().positive().optional(),
     }).parseAsync(query);
   } catch {
     throw new BadRequestError("Paramètres de recherches invalides");
