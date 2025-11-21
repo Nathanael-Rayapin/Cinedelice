1d885e0ac07d19e5bcdcf8cf6473e70681e34ecc
arfaouimehdi <mehdi.arfaoui@oclock.school>
2025-11-21 17:06:36 +0100

    feat: Test POST/api/recipes test create user + 400 age declaration false
    Co-Authored-By: Luc Toscan <luc.toscan@gmail.com>

--- backend/src/controllers/users.controller.spec.test.ts
+++ backend/src/controllers/users.controller.spec.test.ts
@@ -5,7 +5,7 @@ import http from "http";
 import axios from "axios";
 import { app } from "../app.ts"; // <-- important : importer ton serveur Express
 
-
+// Creation d'un user validé
 describe("[POST] /users", () => {
 
   it("should save the user in the database with its password hashed", async () => {
@@ -43,4 +43,39 @@ describe("[POST] /users", () => {
     await prisma.user.delete({ where: { email: USER.email } });
     server.close();
   });
+});
+// Creation d'un user sans declaration d'age
+it("shouldn't save the user in the database with its password hashed if age declaration is false", async () => {
+  // ARRANGE préparation
+  // On creer le server test
+  const server = http.createServer(app);
+  // On démarre un serveur Express juste pour ce test
+  await new Promise<void>((resolve) => server.listen(4001, resolve));
+  // On crée un utilisateur AVEC des valeurs uniques 
+  // pour éviter les doublons en BDD
+  const USER = {
+    username: "lucky_" + Date.now(),// unique
+    email: "luc_" + Date.now() + "@test.com", // unique
+    password: "Password123!",
+    cgu_accepted: true,
+    age_declaration: false
+  };
+  let statusCode = null;
+
+  // ACT agir
+  try{
+    await axios.post("http://localhost:4001/api/users", USER);
+  } catch (error: any) {
+    statusCode = error.response?.status; // axios renvoie l’erreur ici
+  }
+  // ASSERT vérification
+  assert.equal(statusCode, 400,"Tous champs sont obligatoires"); // statut 400 Bad Request
+
+  // Vérifier que l'user n’a PAS été créé
+  const savedUser = await prisma.user.findUnique({
+    where: { email: USER.email }
+  });
+
+  assert.equal(savedUser, null, "Aucun utilisateur ne doit être créé");
+  server.close();
 });
